719d6fe8b79d0722773c89ddeb4874099e0b09ff
beheshti2025hesam-prog <beheshti2025hesam@gmail.com>
2026-09-24 21:16:24 +0330

    Add execution-independent signal contract

---
+++ core/signal/__init__.py
@@ -0,0 +1,4 @@
+"""Mother Agent - execution-independent signal layer."""
+from core.signal.contract import SignalIntent, SignalMode, SignalRecord
+
+__all__ = ["SignalIntent", "SignalMode", "SignalRecord"]
