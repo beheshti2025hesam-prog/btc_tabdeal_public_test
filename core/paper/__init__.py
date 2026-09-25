f1917884b85998fb3f7bd690606e05701656f4a8
beheshti2025hesam-prog <beheshti2025hesam@gmail.com>
2026-09-25 22:22:27 +0330

    Export paper robustness measurement

--- core/paper/__init__.py
+++ core/paper/__init__.py
@@ -1,11 +1,13 @@
 """Execution-free paper-validation contracts."""
 
 from .performance import PaperPerformance, PaperPerformanceResult, PaperTrade
+from .robustness import PaperRobustness, PaperRobustnessResult
 from .session import PaperAction, PaperExit, PaperObservation, PaperSession, PaperState
 from .validation import PaperValidation, PaperValidationResult
 
 __all__ = [
     "PaperAction", "PaperExit", "PaperObservation", "PaperSession", "PaperState",
     "PaperPerformance", "PaperPerformanceResult", "PaperTrade",
+    "PaperRobustness", "PaperRobustnessResult",
     "PaperValidation", "PaperValidationResult",
 ]
