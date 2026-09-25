42e277c1b82de1b5c178d89fd7cbac48e612468b
beheshti2025hesam-prog <beheshti2025hesam@gmail.com>
2026-09-25 15:16:01 +0330

    Add application identity integrity tests

---
+++ tests/test_identity.py
@@ -0,0 +1,10 @@
+from core.identity import APP_NAME, OWNER_NAME, validate_identity
+
+
+def test_canonical_application_identity():
+    assert APP_NAME == "HRS Trade Agent"
+    assert OWNER_NAME == "Seyed Hesameddin Beheshti Shirazi"
+
+
+def test_identity_integrity():
+    validate_identity()
