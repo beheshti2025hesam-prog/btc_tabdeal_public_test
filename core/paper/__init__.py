9d5f89f812601bd703c1a8a9400f25404921c414
beheshti2025hesam-prog <beheshti2025hesam@gmail.com>
2026-09-25 22:08:17 +0330

    Add paper validation contract: core/paper/__init__.py

---
+++ core/paper/__init__.py
@@ -0,0 +1,5 @@
+"""Execution-free paper-validation contracts."""
+
+from .session import PaperAction, PaperObservation, PaperSession, PaperState
+
+__all__ = ["PaperAction", "PaperObservation", "PaperSession", "PaperState"]
