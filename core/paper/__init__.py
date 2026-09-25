5fd6973af05b9df58e73e6d43ef5f4aeb709e81c
beheshti2025hesam-prog <beheshti2025hesam@gmail.com>
2026-09-25 22:19:03 +0330

    Export paper validation contract

--- core/paper/__init__.py
+++ core/paper/__init__.py
@@ -1,5 +1,14 @@
 """Execution-free paper-validation contracts."""
 
 from .session import PaperAction, PaperExit, PaperObservation, PaperSession, PaperState
+from .validation import PaperValidation, PaperValidationResult
 
-__all__ = ["PaperAction", "PaperExit", "PaperObservation", "PaperSession", "PaperState"]
+__all__ = [
+    "PaperAction",
+    "PaperExit",
+    "PaperObservation",
+    "PaperSession",
+    "PaperState",
+    "PaperValidation",
+    "PaperValidationResult",
+]
